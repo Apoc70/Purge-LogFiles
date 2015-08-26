e29e61dae95ba1ab1d31c355f6cfeecb6e14ecd0
Thomas Stensitzki <thomas@mcsmemail.de>
2015-08-26 15:15:47 +0200

    Run-PurgeLogFiles.ps1 added

---
+++ Run-PurgeLogFiles.ps1
@@ -0,0 +1,2 @@
+# Example file to help execute Purge-LogFiles.ps1 with a single PowerShell script, e.g. used for Scheduled Tasks
+.\Purge-LogFiles.ps1 -DaysToKeep 1 -SendMail -MailFrom postmaster@mcsmemail.de -MailTo it@mcsmemail.de -MailServer mail.mcsmemail.de
