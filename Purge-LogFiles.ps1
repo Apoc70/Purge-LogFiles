4be7d4fefeae2873ae07e2eb3f99e46dfa283529
Thomas Stensitzki <thomas@mcsmemail.de>
2016-06-05 17:39:24 +0200

    Minor update due to PowerShell hygiene

--- Purge-LogFiles.ps1
+++ Purge-LogFiles.ps1
@@ -6,10 +6,10 @@
     (Based Based on the original script by Brian Reid, C7 Solutions (c)
     http://www.c7solutions.com/2013/04/removing-old-exchange-2013-log-files-html)
 	
-	THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE 
-	RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
+	  THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE 
+	  RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
 	
-	Version 1.92, 2015-08-26
+	  Version 1.93, 2016-06-05
 
     Ideas, comments and suggestions to support@granikos.eu 
  
@@ -45,13 +45,14 @@
     1.4     Handling of IIS default location fixed
     1.5     Sorting of server names added and Write-Host output changed
     1.6     Count Error fixed
-    1.7		Email report functionality added
-	1.8     Support for global logging and other functions added
+    1.7		  Email report functionality added
+	  1.8     Support for global logging and other functions added
     1.9     Global functions updated (write to event log)
     1.91    Write DaysToKeep to log
     1.92    .Count issue fixed to run on Windows Server 2012
+    1.93    Minor chances to PowerShell hygiene
 	
-	.PARAMETER DaysToKeep
+	  .PARAMETER DaysToKeep
     Number of days Exchange and IIS log files should be retained, default is 30 days
 
     .PARAMETER Auto
@@ -69,7 +70,7 @@
     .PARAMETER MailServer
     SMTP Server for email report
    
-	.EXAMPLE
+	  .EXAMPLE
     Delete Exchange and IIS log files older than 14 days 
     .\Purge-LogFiles -DaysToKeep 14
 
@@ -90,11 +91,11 @@ Param(
     [parameter(Mandatory=$false,ValueFromPipeline=$false,HelpMessage='Send result summary as email')]
         [switch]$SendMail,
     [parameter(Mandatory=$false,ValueFromPipeline=$false,HelpMessage='Sender address for result summary')]
-        [string]$MailFrom = "",
+        [string]$MailFrom = '',
     [parameter(Mandatory=$false,ValueFromPipeline=$false,HelpMessage='Recipient address for result summary')]
-        [string]$MailTo = "",
+        [string]$MailTo = '',
     [parameter(Mandatory=$false,ValueFromPipeline=$false,HelpMessage='SMTP Server address for sending result summary')]
-        [string]$MailServer = ""
+        [string]$MailServer = ''
 )
 
 Set-StrictMode -Version Latest
@@ -112,21 +113,21 @@ Import-Module GlobalFunctions
 $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 $ScriptName = $MyInvocation.MyCommand.Name
 $logger = New-Logger -ScriptRoot $ScriptDir -ScriptName $ScriptName -LogFileRetention 14
-$logger.Write("Script started")
+$logger.Write('Script started')
 
 if($Auto) {
     # detect log file locations automatically an set variables
 
     [string]$ExchangeInstallPath = $env:ExchangeInstallPath
-    [string]$ExchangeUncLogDrive = $ExchangeInstallPath.Split(":\")[0]
-    $ExchangeUncLogPath = $ExchangeUncLogDrive + "$\" + $ExchangeInstallPath.Remove(0,3) + "Logging\"
+    [string]$ExchangeUncLogDrive = $ExchangeInstallPath.Split(':\')[0]
+    $ExchangeUncLogPath = $ExchangeUncLogDrive + "$\" + $ExchangeInstallPath.Remove(0,3) + 'Logging\'
 
     # Fetch local IIS log location from Metabase
     # IIS default location fixed 2015-02-02
-    [string]$IisLogPath = ((Get-WebConfigurationProperty "system.applicationHost/sites/siteDefaults" -Name logFile).directory).Replace("%SystemDrive%",$env:SystemDrive)
+    [string]$IisLogPath = ((Get-WebConfigurationProperty 'system.applicationHost/sites/siteDefaults' -Name logFile).directory).Replace('%SystemDrive%',$env:SystemDrive)
 
     # Extract drive letter and build log path
-    [string]$IisUncLogDrive =$IisLogPath.Split(":\")[0] 
+    [string]$IisUncLogDrive =$IisLogPath.Split(':\')[0] 
     $IisUncLogPath = $IisUncLogDrive + "$\" + $IisLogPath.Remove(0,3) 
 }
 
@@ -136,7 +137,7 @@ Function CleanLogFiles
     Param([string]$Path)
 
     # Build full UNC path
-    $TargetServerFolder = "\\" + $E15Server + "\" + $path
+    $TargetServerFolder = '\\' + $E15Server + '\' + $path
 
     # Write progress bar for current activity
     Write-Progress -Activity "Checking Server $E15Server" -Status "Checking files in $TargetServerFolder" -PercentComplete(($i/$max)*100)
@@ -148,7 +149,7 @@ Function CleanLogFiles
         $LastWrite = $Now.AddDays(-$DaysToKeep)
 
         # Select files to delete
-        $Files = Get-ChildItem $TargetServerFolder -Include *.log -Recurse | Where {$_.LastWriteTime -le "$LastWrite"}
+        $Files = Get-ChildItem $TargetServerFolder -Include *.log -Recurse | Where-Object {$_.LastWriteTime -le "$LastWrite"}
 
         # Lets count the files that will be deleted
         $fileCount = 0
@@ -168,7 +169,7 @@ Function CleanLogFiles
     }
     Else {
         # oops, folder does not exist or is not accessible
-        Write-Host "The folder $TargetServerFolder doesn't exist or is not accessible! Check the folder path!" -ForegroundColor "red"
+        Write-Host "The folder $TargetServerFolder doesn't exist or is not accessible! Check the folder path!" -ForegroundColor 'red'
 
         $Output = "The folder $TargetServerFolder doesn't exist or is not accessible! Check the folder path!"
     }
@@ -178,7 +179,7 @@ Function CleanLogFiles
 
 # Check if we are running in elevated mode
 # function (c) by Michel de Rooij, michel@eightwone.com
-Function Is-Admin {
+Function Get-IsAdmin {
     $currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
     If( $currentPrincipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )) {
         return $true
@@ -188,8 +189,8 @@ Function Is-Admin {
     }
 }
 
-Function Check-SendMail {
-     if( ($SendMail) -and ($MailFrom -ne "") -and ($MailTo -ne "") -and ($MailServer -ne "") ) {
+Function Get-CheckSendMail {
+     if( ($SendMail) -and ($MailFrom -ne '') -and ($MailTo -ne '') -and ($MailServer -ne '') ) {
         return $true
      }
      else {
@@ -198,21 +199,21 @@ Function Check-SendMail {
 }
 
 # Main -----------------------------------------------------
-If (-Not (Check-SendMail)) {
-    Throw "If -SendMail specified, -MailFrom, -MailTo and -MailServer must be specified as well!"
+If (-Not (Get-CheckSendMail)) {
+    Throw 'If -SendMail specified, -MailFrom, -MailTo and -MailServer must be specified as well!'
 }
 
-If (Is-Admin) {
+If (Get-IsAdmin) {
     # We are running in elevated mode. Let's continue.
 
     Write-Output "Removing IIS and Exchange logs - Keeping last $DaysToKeep days - Be patient, it might take some time"
 
     # Track script execution in Exchange Admin Audit Log 
-    Write-AdminAuditLog -Comment "Purge-LogFiles started!"
+    Write-AdminAuditLog -Comment 'Purge-LogFiles started!'
     $logger.Write("Purge-LogFiles started, keeping last $($DaysToKeep) days of log files.")
 
     # Get a list of all Exchange 2013 servers
-    $Ex2013 = Get-ExchangeServer | Where {$_.IsE15OrLater -eq $true} | Sort-Object Name
+    $Ex2013 = Get-ExchangeServer | Where-Object {$_.IsE15OrLater -eq $true} | Sort-Object Name
 
     $logger.WriteEventLog("Script started. Script will purge log files on: $($Ex2013)")
 
@@ -221,9 +222,9 @@ If (Is-Admin) {
     $max = ($Ex2013 | Measure-Object).Count * 2 # two actions to execute per server
 
     # Prepare Output
-    $Output = "<html>
+    $Output = '<html>
     <body>
-    <font size=""1"" face=""Arial,sans-serif"">"
+    <font size="1" face="Arial,sans-serif">'
 
     # Call function for each server and each directory type
     foreach ($E15Server In $Ex2013) {
@@ -238,23 +239,23 @@ If (Is-Admin) {
         $Output += CleanLogfiles -Path $ExchangeUncLogPath
         $i++
 
-        $Output+="</ul>"
+        $Output+='</ul>'
 
     }
 
     # Finalize Output
-    $Output+="</font>
+    $Output+='</font>
     </body>
-    </html>"
+    </html>'
 
     if($SendMail) {
         $logger.Write("Sending email to $($MailTo)")
-        Send-Mail -From $MailFrom -To $MailTo -SmtpServer $MailServer -MessageBody $Output -Subject "Purge-Logfiles Report"         
+        Send-Mail -From $MailFrom -To $MailTo -SmtpServer $MailServer -MessageBody $Output -Subject 'Purge-Logfiles Report'         
     }
 
-    $logger.Write("Script finished")
+    $logger.Write('Script finished')
 }
 else {
     # Ooops, the admin did it again.
-    Write-Output "The script need to be executed in elevated mode. Start the Exchange Management Shell as Administrator."
+    Write-Output 'The script need to be executed in elevated mode. Start the Exchange Management Shell as Administrator.'
 }
